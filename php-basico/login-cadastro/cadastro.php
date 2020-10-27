ace90953c182433e0ed1bedc6817375e4a604deb
Guilherme Avelino <guilhermeavelino1@outlook.com>
2020-10-27 12:18:53 -0300

    Projeto de login com MySQL e PHP

---
+++ php-basico/login-cadastro/cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset='utf-8'>
+    <title>Cadastre-se</title>
+
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/cadastro.css">
+</head>
+<body>
+    <main>
+        <div class="card">
+            <form>
+                <h1>Cadastre-se</h1>
+                <input type="text" placeholder="Digite o seu nome">
+                <input type="email" placeholder="Digite o seu e-mail">
+                <input type="password" placeholder="Digite uma senha">
+                <input type="password" placeholder="Confirme a sua senha">
+                <button><b>Cadastre-se</b></button>
+                <a href="./index.php">Já possui cadastro? Realize o login.</a>            
+            </form>
+        </div>
+    </main>
+</body>
+</html>
---
+++ php-basico/login-cadastro/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset='utf-8'>
+    <title>Entre ou cadastre-se</title>
+
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+<body>
+    <main>
+        <section class="login">
+            <div class="card">
+                <form>
+                    <h1>Entre ou cadastre-se</h1>
+                    <input type="email" placeholder="Digite o seu e-mail">
+                    <input type="password" placeholder="Digite a sua senha">
+                    <button><b>Entrar</b></button>
+                    <a href="./cadastro.php">Ainda não possui cadastro? Clique aqui.</a>
+                </form>
+            </div>
+        </section>
+        <section class="cadastro">
+            <div class="image"></div>
+        </section>
+    </main>
+</body>
+</html>
